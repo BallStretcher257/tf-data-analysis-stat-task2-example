a66a6919217342f7dd992edc989ee84357edc8ef
BallStretcher257 <krasnopeev0407@yandex.ru>
2023-04-05 20:28:32 +0500

    attempt three

--- solution.py
+++ solution.py
@@ -1,5 +1,3 @@
-import math
-
 import pandas as pd
 import numpy as np
 
@@ -12,6 +10,7 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
+    a = np.array([v/4802 for v in x])
     alpha = 1 - p
-    return x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)), \
-           x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))
+    return a.mean() - np.sqrt(np.var(a)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(a)), \
+           a.mean() - np.sqrt(np.var(a)) * norm.ppf(alpha / 2) / np.sqrt(len(a))

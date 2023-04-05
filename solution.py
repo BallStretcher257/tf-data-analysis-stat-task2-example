ce03e49037a0057f38ba85b99178abfaee4446ef
BallStretcher257 <krasnopeev0407@yandex.ru>
2023-04-05 17:42:19 +0500

    attempt two

--- solution.py
+++ solution.py
@@ -3,7 +3,7 @@ import math
 import pandas as pd
 import numpy as np
 
-from scipy.stats import chi2
+from scipy.stats import norm
 
 chat_id = 625760313  # Ваш chat ID, не меняйте название переменной
 
@@ -12,6 +12,6 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    loc = x.mean()*4802
-    return (loc - (2401 + 4802 * math.log(p))), \
-        (loc + (2401 + 4802 * math.log(p)))
+    alpha = 1 - p
+    return x.mean() - np.sqrt(np.var(x)) * norm.ppf(1 - alpha / 2) / np.sqrt(len(x)), \
+           x.mean() - np.sqrt(np.var(x)) * norm.ppf(alpha / 2) / np.sqrt(len(x))

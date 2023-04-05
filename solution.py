afb01f9d6f8c7b94dc8c6705daabc73b7ab85150
BallStretcher257 <krasnopeev0407@yandex.ru>
2023-04-05 16:14:03 +0500

    attempt one fix

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas as pd
 import numpy as np
 
@@ -10,8 +12,6 @@ def solution(p: float, x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 1 - p
-    loc = x.mean()
-    scale = 1 / (2 * x.sum())
-    return loc + scale * chi2.ppf(alpha / 2, 2 * len(x)),\
-        loc + scale * chi2.ppf(1 - alpha / 2, 2 * len(x))
+    loc = x.mean()*4802
+    return (loc - (2401 + 4802 * math.log(p))), \
+        (loc + (2401 + 4802 * math.log(p)))
